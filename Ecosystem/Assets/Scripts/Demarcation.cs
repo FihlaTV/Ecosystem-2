3efcff8fa04728c19bdb75cf405d3494dcad46b9
Lsfelix <lucas.salaverry@edu.pucrs.br>
2019-09-30 17:35:50 -0300

    Functional: Camera Movement / In progress: Demarcation and ImageOpener

---
+++ Ecosystem/Assets/Scripts/Demarcation.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demarcation : MonoBehaviour
+{
+    public GameObject RectanglePrefab;
+
+    private bool creatingArea;
+
+    private Vector3 oldPosition;
+
+    private GameObject newRectangle;
+
+    void Update()
+    {
+        if(creatingArea)
+        {
+
+            var goal = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            var scaleX = goal.x - oldPosition.x;
+            var scaleY = goal.y - oldPosition.y;
+
+            newRectangle.transform.localScale = new Vector3(scaleX,scaleY,1);
+            newRectangle.transform.position += new Vector3(scaleX / 2, scaleY / 2, 0);
+
+            oldPosition = goal;
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                creatingArea = false;
+            }
+        }
+        else if(Input.GetMouseButtonDown(0))
+        {
+
+            Vector3 initialPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            initialPosition.z = -1;
+
+            newRectangle = Instantiate(RectanglePrefab, initialPosition, Quaternion.identity);
+
+            var offsetX = 1.0f;
+            var offsetY = 1.0f;
+
+            newRectangle.transform.position += new Vector3(offsetX, offsetY, 0);
+
+            oldPosition = newRectangle.transform.position;
+
+            creatingArea = true;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Ecosystem/Assets/Scripts/ImageController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SFB;
+
+public class ImageController : MonoBehaviour
+{
+
+    public GameObject imagePlaceholder;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onClick()
+    {
+        openImage();
+    }
+
+    public IEnumerator openImage()
+    {
+        var extensions = new[] {
+                new ExtensionFilter("Image Files", "png", "jpg", "jpeg" )
+            };
+        var path = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensions, false);
+
+        WWW www = new WWW("file:///" + path[0]);
+        while (!www.isDone)
+            yield return null;
+        imagePlaceholder.GetComponent<RawImage>().texture = www.texture;
+    }
+
+}
